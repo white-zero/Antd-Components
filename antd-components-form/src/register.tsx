68f98eea231f3dfcbc2dd0dec7b933c90e20a66d
white-zero <419162177@qq.com>
2020-09-21 09:31:51 +0800

    注册表单

--- antd-components-form/src/register.tsx
+++ antd-components-form/src/register.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState, } from 'react';  
 import {
     Form,
     Input,
@@ -54,9 +55,180 @@ import {
   interface Props {
     form: WrappedFormUtils
   }
-  const RegistrationForm: React.FC<Props>=(props)=>{
+  const RegistrationForm: React.FC<Props>=(props,autoCompleteResult)=>{
 
+    const websiteOptions = autoCompleteResult.map(website => (
+        <AutoCompleteOption key={website}>{website}</AutoCompleteOption>
+      ));
+    const [confirmDirty,setConfirmDirty] =useState(false)
+    const handleSubmit = (e:any) => {
+        e.preventDefault();
+        props.form.validateFieldsAndScroll((err, values) => {
+          if (!err) {
+            console.log('Received values of form: ', values);
+          }
+        });
+      };
+      const handleConfirmBlur = (e:any) => {
+        const { value } = e.target;
+        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+      };
+    const compareToFirstPassword = (rule, value, callback) => {
+        const { form } = props;
+        if (value && value !== form.getFieldValue('password')) {
+          callback('Two passwords that you enter is inconsistent!');
+        } else {
+          callback();
+        }
+      };
+    const validateToNextPassword = (rule, value, callback) => {
+        const { form } = props;
+        if (value && confirmDirty) {
+          form.validateFields(['confirm'], { force: true });
+        }
+        callback();
+      };
+      const handleWebsiteChange = value => {
+        let autoCompleteResult;
+        if (!value) {
+          autoCompleteResult = [];
+        } else {
+          autoCompleteResult = ['.com', '.org', '.net'].map(domain => `${value}${domain}`);
+        }
+        this.setState({ autoCompleteResult });
+      };
+   
+    const { getFieldDecorator } = props.form;
+    const formItemLayout = {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 8 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+      };
+      const tailFormItemLayout = {
+        wrapperCol: {
+          xs: {
+            span: 24,
+            offset: 0,
+          },
+          sm: {
+            span: 16,
+            offset: 8,
+          },
+        },
+      };
       return(
-
+        <Form {...formItemLayout} onSubmit={handleSubmit}>
+          <Form.Item label="E-mail">
+            {getFieldDecorator('email', {
+              rules: [
+                {
+                  type: 'email',
+                  message: 'The input is not valid E-mail!',
+                },
+                {
+                  required: true,
+                  message: 'Please input your E-mail!',
+                },
+              ],
+            })(<Input />)}
+          </Form.Item>
+          <Form.Item label="Password" hasFeedback>
+            {getFieldDecorator('password', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+                {
+                  validator: validateToNextPassword,
+                },
+              ],
+            })(<Input.Password />)}
+          </Form.Item>
+          <Form.Item label="Confirm Password" hasFeedback>
+            {getFieldDecorator('confirm', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please confirm your password!',
+                },
+                {
+                  validator: compareToFirstPassword,
+                },
+              ],
+            })(<Input.Password onBlur={handleConfirmBlur} />)}
+          </Form.Item>
+          <Form.Item
+            label={
+              <span>
+                Nickname&nbsp;
+                <Tooltip title="What do you want others to call you?">
+                  <Icon type="question-circle-o" />
+                </Tooltip>
+              </span>
+            }
+          >
+            {getFieldDecorator('nickname', {
+              rules: [{ required: true, message: 'Please input your nickname!', whitespace: true }],
+            })(<Input />)}
+          </Form.Item>
+          <Form.Item label="Habitual Residence">
+            {getFieldDecorator('residence', {
+              initialValue: ['zhejiang', 'hangzhou', 'xihu'],
+              rules: [
+                { type: 'array', required: true, message: 'Please select your habitual residence!' },
+              ],
+            })(<Cascader options={residences} />)}
+          </Form.Item>
+          <Form.Item label="Phone Number">
+            {getFieldDecorator('phone', {
+              rules: [{ required: true, message: 'Please input your phone number!' }],
+            })(<Input addonBefore={prefixSelector} style={{ width: '100%' }} />)}
+          </Form.Item>
+          <Form.Item label="Website">
+            {getFieldDecorator('website', {
+              rules: [{ required: true, message: 'Please input website!' }],
+            })(
+              <AutoComplete
+                dataSource={websiteOptions}
+                onChange={handleWebsiteChange}
+                placeholder="website"
+              >
+                <Input />
+              </AutoComplete>,
+            )}
+          </Form.Item>
+          <Form.Item label="Captcha" extra="We must make sure that your are a human.">
+            <Row gutter={8}>
+              <Col span={12}>
+                {getFieldDecorator('captcha', {
+                  rules: [{ required: true, message: 'Please input the captcha you got!' }],
+                })(<Input />)}
+              </Col>
+              <Col span={12}>
+                <Button>Get captcha</Button>
+              </Col>
+            </Row>
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout}>
+            {getFieldDecorator('agreement', {
+              valuePropName: 'checked',
+            })(
+              <Checkbox>
+                I have read the <a href="">agreement</a>
+              </Checkbox>,
+            )}
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
       )
   }
